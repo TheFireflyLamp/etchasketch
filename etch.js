17498440ebcd3f246d795f403037029a29687538
Viola Sim <ke.svxust@gmail.com>
2018-08-16 22:50:02 +0800

    working clear and resize buttons

--- etch.js
+++ etch.js
@@ -1,5 +1,5 @@
 const gridArea = document.querySelector('#gridArea');
-let userInput = 20;
+let userInput = 10;
 let userSquared = userInput*userInput;
 let basisValue = 600/userInput;
 let refBasisValue = basisValue + "px";
@@ -7,9 +7,39 @@ console.log (refBasisValue);
 console.log (userInput);
 console.log (userSquared);
 
-for (let i = 1; i<userSquared; i++) {
-  const grid = document.createElement('div');
-  grid.classList.add("gridCell");
-  grid.style.width= refBasisValue;
-  gridArea.appendChild(grid);
+createGrid (userInput);
+
+function updateValues(){
+  userSquared = userInput*userInput;
+  basisValue = 600/userInput;
+  refBasisValue = basisValue + "px";
+}
+
+function createGrid (userInput) { 
+  for (let i = 1; i<userSquared; i++) {
+    const grid = document.createElement('div');
+    grid.classList.add("gridCell");
+    grid.style.width= refBasisValue;
+    gridArea.appendChild(grid);
+  }
+}
+
+function changeGrid() {
+  let value = prompt("Please enter how many squares wide you want your canvas to be. The maximum value accepted is 60.",30);
+  while(gridArea.firstChild){
+    gridArea.removeChild(gridArea.firstChild);}
+  userInput = parseInt(value);
+  updateValues();
+  console.log(userInput);
+  createGrid(userInput);
+}
+
+function clearGrid() {
+  let nodes = document.getElementById('gridArea').childNodes;
+  for(var i=0; i<nodes.length; i++) {
+    if (nodes[i].nodeName.toLowerCase() == 'div') {
+         nodes[i].style.backgroundColor = 'white';
+     }
+}
+  console.log(typeof (grid));
 }
